f2104c1c389cffccc3391bb756a59da93222433d
harper71 <princeazuka99@gmail.com>
2024-01-09 17:48:36 -0800

    task from 1-8 commet message two

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
 #endif 
