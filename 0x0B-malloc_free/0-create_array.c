0c76d26f334cb70d0daec948a4a7119ad92ca940
harper71 <princeazuka99@gmail.com>
2024-01-16 04:36:10 -0800

    malloc commit 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates a string of carracter
+ * @size: this is the number of character
+ * @c: the character array
+ * Return: arrSTR always
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arrSTR = (char *)malloc(size * sizeof(char));
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	while (i < size)
+	{
+		arrSTR[i] = c;
+		i++;
+	}
+	return (arrSTR);
+}
