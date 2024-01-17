e19447012cfee4443cbcd103dd978985feaa1c91
harper71 <princeazuka99@gmail.com>
2024-01-17 12:58:19 -0800

    commit on task 1 again

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * _strlen - used to check the length of the string
+ * @s: points to a character or a string of characters
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (1);
+}
 /**
  * _strdup - duplicates a string of characters
  * @str: character to be dulicaterd
@@ -9,7 +24,7 @@
  */
 char *_strdup(char *str)
 {
-	int l = 0;
+	int l = _strlen(str);
 
 	int x = 0;
 
@@ -19,12 +34,13 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	val5 = (char *)malloc((l + 1) * sizeof(char));
+	val5 = (char *)malloc(l * sizeof(char));
 
 	while (str[l] != '\0')
 	{
 		l++;
 	}
+
 	if (val5 != NULL)
 	{
 		for (; x < l ; x++)
