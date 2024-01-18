54893046fc5facd9bc1607f1a38147989345d30e
harper71 <princeazuka99@gmail.com>
2024-01-18 14:28:03 -0800

    task 2 commit 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: array name
+ * @size: array size
+ * Return: arr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+
+	char *arr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb * size; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
