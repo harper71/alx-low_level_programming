669a546002034cf460d57e694cb3a8c8e8586993
harper71 <princeazuka99@gmail.com>
2024-01-22 06:27:28 -0800

    structure task commit(2)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,12 +7,11 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: dog's owner's name
+ * Return: 0 always
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *da = malloc(sizeof(d));
 	d->name = name;
 	d->age = age;
-	d.owner = owner;
-	return (d);
+	d->owner = owner;
 }
