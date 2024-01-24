278b58f58015dc7fdbfaadb8355a9693101407b6
harper71 <princeazuka99@gmail.com>
2024-01-24 04:06:18 -0800

    task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,19 +9,3 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
-/**
- * name_1 - name if the person
- * @name: the name
- * Return: nothing
- */
-void name_1(char *name)
-{
-	int i = 0;
-
-	while (name[i] != '\n' && name[i] != '\0')
-	{
-		_putchar(name[i]);
-		i++;
-	}
-	_putchar('\n');
-}
