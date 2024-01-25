a7e1c26a9707b9df0369c13658d5110eb9ed867a
harper71 <princeazuka99@gmail.com>
2024-01-25 05:10:35 -0800

    commit one on task 1and2

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_number - print the listed numbers
+ * @separator: used to seperate two numbers
+ * @n: the total amount of number that is printed
+ * @...: the numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL)
+	{
+		va_end(args);
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		int nums = va_arg(args, int);
+
+		printf("%d", nums);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - print the listed numbers
+ * @separator: used to seperate two numbers
+ * @n: the total amount of number that is printed
+ * @...: the numbers to be printed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL)
+	{
+		va_end(args);
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		const char *nums = va_arg(args, const char *);
+
+		if (nums == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", nums);
+		}
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
