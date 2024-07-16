969abe00fe87b7959c57f743ac33163898201713
harper71 <princeazuka99@gmail.com>
2024-07-16 17:53:27 +0100

    commit changes

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,9 @@
+#include "search_algos.h"
+/**
+ * linear_search - create a linear search algorithm
+ * @array: pointer to an array of elements
+ * @size: size of the array
+ * @value: value to search for
+ * Return: value of the element
+ */
+
