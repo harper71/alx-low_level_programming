58c7b0247d23ace7e761919f92c3fd68482dd208
harper71 <princeazuka99@gmail.com>
2024-07-17 19:21:12 +0100

    commit task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,10 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (value == array[i])
+		{
 			return (i);
+			break;
+		}
 	}
 	return (-1);
 }
